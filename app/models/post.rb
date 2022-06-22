3acd6c07e245892638239aa08f496d74286ccdb8
Albert Byrone <albertbyrone1677@gmail.com>
2022-06-22 11:26:26 +0300

    commit tree bc255ef5f15278552a4c96fa59bf20960689994c

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250}
+  validates :summary, length: { maximum: 250}
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction']}
+
+  PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+  ]
+  def clickbait?
+    if PATTERNS.none? { |p| p.matct title}
+      errors.add(:title, 'must be clickbait')
+    end
+  end
 end
